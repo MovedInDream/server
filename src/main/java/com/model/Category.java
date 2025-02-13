49b0278fa96e60530d619a51268f1931f087bfcd
zhangshuaiyu <zhangshuaiyu1169@foxmail.com>
2025-02-13 20:26:32 +0800

    add models

---
+++ src/main/java/com/model/Category.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import java.time.LocalDateTime;
+
+public class Category {
+    private Integer id;
+    private String name;
+    private String alias;
+    private Integer createUser;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
